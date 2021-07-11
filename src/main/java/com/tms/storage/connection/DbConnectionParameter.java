36187e882e5a9ecef9c636e4bfba26bba6fbecae
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-07-11 20:05:39 +0300

    Update DbConnectionParameter.java

--- src/main/java/com/tms/storage/connection/DbConnectionParameter.java
+++ src/main/java/com/tms/storage/connection/DbConnectionParameter.java
@@ -3,7 +3,7 @@ package com.tms.storage.connection;
 public class DbConnectionParameter {
     private final String url = "jdbc:postgresql://localhost:5432/postgres";
     private final String user = "postgres";
-    private final String password = "jxhpw5GZq6J";
+    private final String password = "PASSWORD";
 
     public String getUrl() { return url; }
 
